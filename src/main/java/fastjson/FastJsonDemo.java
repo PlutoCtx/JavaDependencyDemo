4f19b8919b7e833b383899e23daa632030912843
PlutoCtx <chentingxian195467@163.com>
2024-07-19 20:50:22 +0800

    Initial commit

---
+++ src/main/java/fastjson/FastJsonDemo.java
@@ -0,0 +1,13 @@
+package fastjson;
+/**
+ * JavaDependencyDemo
+ * FastJSON使用
+ * 
+ * @author  PlutoCtx 
+ * @email   ctx195467@163.com
+ * @version 2024/7/19 20:01
+ * @since   JDK17
+ */
+
+public class FastJsonDemo {
+}
---
+++ src/main/java/fastjson/User.java
@@ -0,0 +1,13 @@
+package fastjson;
+/**
+ * JavaDependencyDemo
+ * 用户 demo
+ * 
+ * @author  PlutoCtx 
+ * @email   ctx195467@163.com
+ * @version 2024/7/19 19:28
+ * @since   JDK17
+ */
+
+public class User {
+}
